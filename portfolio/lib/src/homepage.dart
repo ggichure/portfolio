50d4a18f52b963a14d9e24b604366bd16bb1e136
griffins <g.gichure@outlook.com>
2020-02-28 16:02:28 +0300

    💩 trying new layout

--- portfolio/lib/src/homepage.dart
+++ portfolio/lib/src/homepage.dart
@@ -1,22 +1,94 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/src/appthemes.dart';
 import 'dart:js' as js;
 import 'package:portfolio/src/responsivewidget.dart';
 import 'package:portfolio/src/smallscreens/smallscreen.dart';
+import 'package:portfolio/src/tabs.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin, SingleTickerProviderStateMixin {
+  final List<String> imgList = [
+    "assets/3.jpg",
+  ];
+  //circle color blinker
+  Animation<Color> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 4500), vsync: this);
+    final CurvedAnimation curve =
+        CurvedAnimation(parent: controller, curve: Curves.linear);
+    animation =
+        ColorTween(begin: Colors.green, end: Colors.white).animate(curve);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveWidget(
       largeScreen: Scaffold(
-        body: ResponsiveWidget.isSmallScreen(context)
-            ? smallScreen()
-            : largeScreen(),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                child: AnimatedBuilder(
+                    animation: animation,
+                    builder: (BuildContext context, Widget child) {
+                      return Container(
+                          height: 140,
+                          width: 160,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                          ),
+                          child: CarouselSlider(
+                            autoPlay: true,
+                            autoPlayAnimationDuration:
+                                Duration(milliseconds: 3000),
+                            items: imgList.map((url) {
+                              return Image.asset(
+                                url,
+                                scale: 2.0,
+                                fit: BoxFit.fill,
+                                filterQuality: FilterQuality.high,
+                              );
+                            }).toList(),
+                          ));
+                    })),
+                    SizedBox(height: 10,),
+            Text(
+              """
+Hi, I'm Griffins
+Frontend devloper with a passion in ML and AI .
+I make stuff happen. \n
+Talk to me about tech, planes,football,space and supercars.
+""",
+              style: AppThemeStyles.textboldHugeW,
+            ),
+            Tabs(),
+          ],
+        ),
         bottomSheet: Container(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -27,103 +99,6 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-    );
-  }
-
-  Widget largeScreen() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(8),
-          width: MediaQuery.of(context).size.width * .70,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(8),
-                      width: MediaQuery.of(context).size.width * .8,
-                      child: Text(
-                        """
-Hi, I'm Griffins
-Frontend devloper with a passion in ML and AI .
-I make stuff happen. \n
-Talk to me about tech, planes,football,space and supercars.
-""",
-                        style: AppThemeStyles.textboldHugeW,
-                      ),
-                    ),
-                  ],
-                ),
-                Center(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text("Current dev stack  "),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      currentStack(),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text("Technologies I have used "),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      techUsed(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      social(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          js.context.callMethod("open", [
-                            "https://stackoverflow.com/users/10409567/g-griffo"
-                          ]);
-                        },
-                        child: Image.network(
-                          "https://stackexchange.com/users/flair/14410660.png",
-                          height: 58,
-                          width: 208,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      FloatingActionButton.extended(
-                        hoverColor: Colors.amberAccent,
-                        icon: Image.asset(
-                          "assets/gith.png",
-                          height: 50,
-                          width: 50,
-                        ),
-                        label: Text("Source Code"),
-                        onPressed: () {
-                          js.context.callMethod("open",
-                              ["https://github.com/ggichure/portfolio"]);
-                        },
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 
@@ -446,4 +421,7 @@ Talk to me about tech, planes,football,space and supercars.
       ],
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- portfolio/lib/src/tabs.dart
+++ portfolio/lib/src/tabs.dart
@@ -1,5 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'dart:js' as js;
+
+import 'data/projects.dart';
+import 'smallscreens/smallscreen.dart';
 
 class Tabs extends StatelessWidget {
   @override
@@ -19,29 +22,30 @@ class Tabs extends StatelessWidget {
                     indicatorColor: Colors.orangeAccent,
                     unselectedLabelColor: Colors.grey,
                     labelPadding: EdgeInsets.only(
-                      bottom: 15,
+                      bottom: 8,
+                      top: 8,
                     ),
-                    indicatorWeight: 3.5,
-                    labelColor: Colors.black,
+                    indicatorWeight: 4.5,
+                    labelColor: Colors.white,
                     labelStyle: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 18,
                     ),
                     tabs: <Widget>[
                       Container(
-                        width: 140,
+                        width: 180,
                         child: Center(
                           child: Text("Current Stack"),
                         ),
                       ),
                       Container(
-                        width: 140,
+                        width: 180,
                         child: Center(
-                          child: Text("previous stack"),
+                          child: Text("Tech used"),
                         ),
                       ),
                       Container(
-                        width: 140,
+                        width: 180,
                         child: Center(
                           child: Text("projects"),
                         ),
@@ -54,17 +58,220 @@ class Tabs extends StatelessWidget {
                       ),
                     ]),
               ),
-              Container(
+              Container(alignment: Alignment.topCenter,
                 margin: EdgeInsets.symmetric(vertical: 25),
                 padding: EdgeInsets.symmetric(horizontal: 0),
+                width:  MediaQuery.of(context).size.height * .60,
                 height: MediaQuery.of(context).size.height * .70,
                 child: TabBarView(children: <Widget>[
+                  currentStack(),
                   
-              
+                  techUsed(),
+                  Text("coming soon"),
+                  social(),
                 ]),
               )
             ],
           )),
+    );
+  }
+
+  Widget techUsed() {
+    return Wrap(
+      spacing: 14,
+      children: <Widget>[
+        Chip(
+          label: Text("Webpack"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("jQuery"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("PHP"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("SQL"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("MVC"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("React"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("MYSQL"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("Bootsrap"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("Wordpress"),
+          shape: StadiumBorder(),
+        ),
+        Chip(
+          label: Text("e.t.c"),
+          shape: StadiumBorder(),
+        ),
+      ],
+    );
+  }
+
+  Widget currentStack() {
+    return Wrap(
+      spacing: 4,
+      children: <Widget>[
+        Chip(
+          label: Text("Flutter"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.blue,
+        ),
+        Chip(
+          label: Text("Android"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.teal,
+        ),
+        Chip(
+          label: Text("Firebase"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.amberAccent,
+        ),
+        Chip(
+          label: Text("Node Js"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.green,
+        ),
+        Chip(
+          label: Text("Javascript"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.blueGrey,
+        ),
+        Chip(
+          label: Text("iOS"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.grey,
+        ),
+        Chip(
+          label: Text("Kotlin"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.purpleAccent,
+        ),
+        Chip(
+          label: Text("Java"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        Chip(
+          label: Text("Python"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.blue,
+        ),
+        Chip(
+          label: Text("Mongo DB"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.lime,
+        ),
+        Chip(
+          label: Text("Tensor Flow"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.deepOrange,
+        ),
+        Chip(
+          label: Text("GCP"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.orangeAccent,
+        ),
+        Chip(
+          label: Text(" OpenVINO"),
+          shape: StadiumBorder(),
+          backgroundColor: Colors.blue[200],
+        ),
+      ],
+    );
+  }
+
+  Widget social() {
+    return Wrap(
+      spacing: 5,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            js.context.callMethod("open", ["https://github.com/ggichure"]);
+          },
+          child: Tab(
+            icon: Image.asset(
+              "assets/gith.png",
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        GestureDetector(
+          onTap: () {
+            js.context.callMethod(
+                "open", ["https://stackoverflow.com/users/10409567/g-griffo"]);
+          },
+          child: Tab(
+            icon: Image.asset(
+              "assets/so.png",
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        GestureDetector(
+          onTap: () {
+            js.context.callMethod("open", ["https://twitter.com/Ggriffo68"]);
+          },
+          child: Tab(
+            icon: Image.asset(
+              "assets/twitter.png",
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        GestureDetector(
+          onTap: () {
+            js.context.callMethod("open", ["https://medium.com/@Ggriffo"]);
+          },
+          child: Tab(
+            icon: Image.asset(
+              "assets/medium.png",
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            js.context.callMethod("open", ["https://t.me/g_ggriffo"]);
+          },
+          child: Tab(
+            icon: Image.asset(
+              "assets/tl.png",
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
